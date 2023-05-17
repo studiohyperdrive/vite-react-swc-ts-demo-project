f9ccf4acd26cef7a0f38f4c3f73d15eba8e8275e
smtsj <jo.smets@studiohyperdrive.be>
2023-05-17 15:29:18 +0200

    chore: alter project description

--- src/modules/app/views/Home/Home.tsx
+++ src/modules/app/views/Home/Home.tsx
@@ -10,9 +10,11 @@ export const Home: FC = () => {
       </Text>
       <Text mt="60px">
         Deze voorbeeldapplicatie bevat een Art-pagina die kunstwerken gaat
-        ophalen van een publieke API. Onderaan de pagina staat een knop om meer
-        kunstwerken in te laden. Bij het klikken op een resultaat word je
-        doorverwezen naar een detail pagina.
+        ophalen van een{" "}
+        <a href="https://api.artic.edu/docs/#introduction">publieke API</a>.
+        Onderaan de pagina staat een knop om meer kunstwerken in te laden. Bij
+        het klikken op een resultaat word je doorverwezen naar een detail
+        pagina.
       </Text>
       <Text mt="30px">
         Het ophalen en bewaren van data in de store doen we aan de hand van{" "}
@@ -20,13 +22,23 @@ export const Home: FC = () => {
         <a href="https://redux-toolkit.js.org/">React Redux Toolkit</a>.
       </Text>
       <Text mt="30px">
-        Ga zelf aan de slag om de Exhibitions pagina verder uit te werken,
-        gelijkaardig aan de Art pagina. In grote lijnen dienen volgende stappen
-        ondernomen te worden:
+        Ga zelf aan de slag om het Exhibitions overzicht en detail verder uit te
+        werken, gelijkaardig aan de Art pagina&apos;s. Gebruik op de
+        overzichtspagina een tabel (beschikbaar in mantine) met de titel,
+        status, startdatum en einddatum van de exhibitions. Onderdaan de pagina
+        bevindt zich een &quot;laad meer&quot;-knop. Op de tabel kan je
+        doorklikken om naar het detail van een exhibition te gaan. Op de
+        detailpagina toon je buiten de titel, status, start- en einddatum ook de
+        afbeelding en de beschrijving van de exhibition.
+      </Text>
+      <Text mt="30px">
+        In grote lijnen dienen volgende stappen ondernomen te worden:{" "}
       </Text>
       <ul>
         <li>Opzetten exhibitions module</li>
-        <li>Exhibitions overzicht en detail routing en pagina&apos;s voorzien</li>
+        <li>
+          Exhibitions overzicht en detail routing en pagina&apos;s voorzien
+        </li>
         <li>Exhibitions slice voorzien</li>
         <li>(Async) actions/thunks en reducers voorzien</li>
         <li>Actions dispatchen</li>
